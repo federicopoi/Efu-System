4843840605422fe591c89d682f98d06b0557c3df
Federico Poi <fedepoi@MacBook-Pro-de-Federico.local>
2020-07-16 20:14:15 -0300

    make it better

--- server.js
+++ server.js
@@ -14,24 +14,31 @@ app.use(express.json());
 
 // DB Config
 const db = config.get("mongoURI");
+const port = process.env.PORT || 5000;
 
 // Connect to Mongo
 
 mongoose
-  .connect(db, {
+  .connect(process.env.MONGODB_URI || db, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
   })
   .then(() => console.log("MongoDB Connected..."))
   .catch((err) => console.log(err));
-
 // User routes
 app.use("/api/tarjetas", tarjetas);
 app.use("/api/filters", filters);
 app.use("/api/users", users);
 app.use("/api/auth", auth);
 
-const port = process.env.PORT || 5000;
+// Step 3
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html")); // relative path
+  });
+}
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
