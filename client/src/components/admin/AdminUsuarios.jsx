84363c9a90ec9328447b46d6b5d3c23b4df22bbc
Federico Poi <fedepoi@MBP-de-Federico.lan>
2020-11-25 21:16:26 -0600

    11-25-2020

--- client/src/components/admin/AdminUsuarios.jsx
+++ client/src/components/admin/AdminUsuarios.jsx
@@ -46,12 +46,11 @@ export class AdminUsuarios extends Component {
             {this.props.user && this.props.user.role === "Admin" && (
               <Container>
                 <Row>
-                  s
                   <Col>
                     <div className="d-sm-flex align-items-center">
                       <div className="">
                         <div>
-                          <h2 className="mb-3">Administrar usuarios</h2>
+                          <h2 className="mb-3">Administrar</h2>
                         </div>
                       </div>
                       <div className="ml-auto d-sm-flex no-block align-items-center mb-3">
